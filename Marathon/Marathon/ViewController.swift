2a6175e59207e4613c446823ea694f8e71111ca1
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2023-06-26 09:40:56 +0300

    init

---
+++ Marathon/Marathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon
+//
+//  Created by Nikita Izosimov on 05.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
