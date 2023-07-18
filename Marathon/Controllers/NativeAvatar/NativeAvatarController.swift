40d2306029dc4164e65da5921742d1ac8a935c8a
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2023-07-18 18:09:33 +0300

    add NativeAvatarController

--- Marathon/Controllers/NativeAvatar/NativeAvatarController.swift
+++ Marathon/Controllers/NativeAvatar/NativeAvatarController.swift
@@ -9,5 +9,75 @@ import UIKit
 
 final class NativeAvatarController: UIViewController {
     
-
+    // MARK: - Views
+    
+    private lazy var avatarContainerView: UIView = { UIView() }()
+    
+    private lazy var avatarView: UIImageView = {
+        let view = UIImageView()
+        
+        view.image = UIImage(systemName: "person.crop.circle.fill")
+        
+        view.tintColor = .systemGray
+        
+        view.widthAnchor.constraint(equalToConstant: 36).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 36).isActive = true
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        
+        view.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 2)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigation()
+        setupViews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setupNativeAvatar()
+    }
+    
+    // MARK: - Setup Views
+    
+    private func setupNavigation() {
+        title = "Avatar"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupNativeAvatar() {
+        guard let view = navigationController?.navigationBar
+            .subviews
+            .first(where: { $0.description.contains("UINavigationBarLargeTitleView") }) else { return }
+        
+        view.addSubview(avatarView)
+        avatarView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        avatarView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(scrollView)
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
