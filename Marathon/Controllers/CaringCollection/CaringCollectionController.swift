39a9807c3bc1175685ac643f5feef36ecd690ded
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2023-07-21 18:29:46 +0300

    clear

--- Marathon/Controllers/CaringCollection/CaringCollectionController.swift
+++ Marathon/Controllers/CaringCollection/CaringCollectionController.swift
@@ -33,7 +33,6 @@ final class CaringCollectionController: UIViewController {
         
         view.showsHorizontalScrollIndicator = false
         
-        
         view.dataSource = self
         view.delegate = self
         
